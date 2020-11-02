329477e68425e77d9b57007e1d81af54bc373e7d
Lucky Rai <luckrai9@gmail.com>
2020-11-02 15:16:46 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Recipes from "./components/Recipes";
+import Axios from "axios";
 
-function App() {
+const App = () => {
+  const [search, setSearch] = useState("chiken");
+  const [recipes, setRecipes] = useState([]);
+
+  const APP_ID = "c772609a";
+  const APP_KEY = "a520368f16e95b9d3a66b9d59db9bb96";
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const res = await Axios.get(
+      `https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+
+    setRecipes(res.data.hits);
+  };
+  const onInputChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const onSearchClick = () => {
+    getRecipes();
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header
+        search={search}
+        onInputChange={onInputChange}
+        onSearchClick={onSearchClick}
+      />
+      <div className="container">
+        <Recipes recipes={recipes} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Header = (props) => {
+  const { search, onInputChange, onSearchClick } = props;
+  return (
+    <div className="jumbotron">
+      <h1 className="display-1">
+        <span className="material-icons brand-icon" brand-icon>
+          fastfood
+        </span>
+        Food Recipe
+      </h1>
+      <div className="input-group w-50 mx-auto ">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search Your Favourite Recipe..."
+          value={search}
+          onChange={onInputChange}
+        />
+        <div className="input-group-append">
+          <button className="btn btn-dark" onClick={onSearchClick}>
+            Search Recipe
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Header;
